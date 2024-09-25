f887260ed11dfaba9c5c3786681bb404280eefdf
parkyodo <scarletdemon@naver.com>
2024-09-25 18:36:29 +0900

    feat: provider observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state/riverpod/provider_observer.dart';
 import 'package:state/screen/home_screen.dart';
 
 void main() {
-  runApp(const ProviderScope(
-    child: MaterialApp(
+  runApp(ProviderScope(
+    observers: [Logger()],
+    child: const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
