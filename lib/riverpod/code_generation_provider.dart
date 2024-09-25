bb703c1daaeb15529a9f638967960e51b5272e96
parkyodo <scarletdemon@naver.com>
2024-09-25 19:40:15 +0900

    feat: riverpod code generation keepAlive

--- lib/riverpod/code_generation_provider.dart
+++ lib/riverpod/code_generation_provider.dart
@@ -6,3 +6,17 @@ part 'code_generation_provider.g.dart';
 String gState(GStateRef ref) {
   return 'Hello Code Generation';
 }
+
+@riverpod
+Future<int> gStateFuture(GStateFutureRef ref) async {
+  await Future.delayed(const Duration(seconds: 2));
+
+  return 10;
+}
+
+@Riverpod(keepAlive: true)
+Future<int> gStateFuture2(GStateFuture2Ref ref) async {
+  await Future.delayed(const Duration(seconds: 2));
+
+  return 10;
+}
--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -9,11 +9,29 @@ class CodeGenerationScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFuture2Provider);
 
     return DefaultLayout(
         title: 'CodeGenerationScreen',
         body: Column(
-          children: [Text('state1 : $state1')],
+          children: [
+            Text('state1 : $state1'),
+            state2.when(
+                data: (data) => Text(
+                      'state2 : $data',
+                      textAlign: TextAlign.center,
+                    ),
+                error: (err, stack) => Text(err.toString()),
+                loading: () => const CircularProgressIndicator()),
+            state3.when(
+                data: (data) => Text(
+                      'state3 : $data',
+                      textAlign: TextAlign.center,
+                    ),
+                error: (err, stack) => Text(err.toString()),
+                loading: () => const CircularProgressIndicator())
+          ],
         ));
   }
 }
